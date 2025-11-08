12ed9b7906243eb9ca25ef0c59410cf5c2c8db57
Max <vosroix@gmail.com>
2025-11-08 22:06:49 +0500

    added gtag

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <MainLayout/>
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-HPLFC60KZW"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-HPLFC60KZW');
+  </script>
 </template>
 
 <script lang="ts" setup>
